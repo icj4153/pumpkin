11c543c31530144d8b686dee8085a2a7d8754197
Im, Changjun <115138216+icj4153@users.noreply.github.com>
2025-05-28 01:35:43 +0900

    포트 수정

--- app.py
+++ app.py
@@ -75,4 +75,6 @@ def convert():
                      mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
